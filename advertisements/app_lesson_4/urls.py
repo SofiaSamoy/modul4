5161ce706f5884e6626a2f91473b140703204bb5
Sofia <sofia@gmail.com>
2023-08-01 19:32:13 +0300

    дз к 5му занятию

--- advertisements/app_lesson_4/urls.py
+++ advertisements/app_lesson_4/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
-from .views import index
+from .views import index, top_sellers
 
-urlpatterns = [path('lesson_4', index)]
+urlpatterns = [path('', index, name = 'main-page'),
+               path('top-sellers/', top_sellers, name = 'top-sellers'),]
